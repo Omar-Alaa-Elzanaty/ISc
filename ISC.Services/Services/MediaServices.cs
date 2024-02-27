b5a53ec9bcc4439f87da1b9024b7d9a8bdc44113
Omar Alaa <Omaralaaelzanaty@gmail.com>
2024-02-27 14:50:15 +0200

    Edit MediaService

--- ISC.Services/Services/MediaServices.cs
+++ ISC.Services/Services/MediaServices.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,38 +12,58 @@ namespace ISC.Services.Services
 {
 	public class MediaServices:IMediaServices
 	{
-		private readonly IWebHostEnvironment _Host;
-		private readonly IHttpContextAccessor _HttpContext;
+		private readonly IWebHostEnvironment _host;
+		private readonly HttpContext _httpContext;
 
-		public MediaServices(IWebHostEnvironment host, IHttpContextAccessor httpContext)
+		public MediaServices(IWebHostEnvironment host, IHttpContextAccessor context)
 		{
-			_Host = host;
-			_HttpContext = httpContext;
+			_host = host;
+			_httpContext = context.HttpContext;
 		}
 		public async Task<string> AddAsync(IFormFile media)
 		{
-			string RootPath = _Host.WebRootPath;
-			string FileName = Guid.NewGuid().ToString();
-			string Extension = Path.GetExtension(media.FileName);
-			string MediaFolderPath = "";
-			bool isImage = false;
-			if (isImageExtension(Extension))
-			{
-				MediaFolderPath = Path.Combine(RootPath, "Images");
-				isImage = true;
-			}
-			using (FileStream fileStreams = new(Path.Combine(MediaFolderPath,
-											FileName + Extension), FileMode.Create))
-			{
-				media.CopyTo(fileStreams);
-				fileStreams.Flush();
-				fileStreams.Dispose();
-				fileStreams.Close();
-			}
-			if (isImage)
-				return @$"{_HttpContext.HttpContext?.Request.Scheme}://{_HttpContext?.HttpContext?.Request.Host}/Images/" + FileName + Extension;
-			else
-				return @$"{_HttpContext.HttpContext?.Request.Scheme}://{_HttpContext?.HttpContext?.Request.Host}/Records/" + FileName + Extension;
+            var extension = Path.GetExtension(media.FileName).ToLower();
+
+            var uniqueFileName = Guid.NewGuid().ToString() + extension;
+
+            var uploadsFolder = Path.Combine("wwwroot", "Images");
+
+            if (!Directory.Exists(uploadsFolder))
+            {
+                Directory.CreateDirectory(uploadsFolder);
+            }
+
+            var filePath = Path.Combine(uploadsFolder, uniqueFileName);
+            using (var stream = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                media.CopyTo(stream);
+                stream.Dispose();
+            }
+            var baseUrl = @$"{_httpContext.Request.Scheme}://{_httpContext.Request.Host}/Images/";
+
+            return baseUrl + uniqueFileName;
+   //         string RootPath = _Host.WebRootPath;
+			//string FileName = Guid.NewGuid().ToString();
+			//string Extension = Path.GetExtension(media.FileName);
+			//string MediaFolderPath = "";
+			//bool isImage = false;
+			//if (isImageExtension(Extension))
+			//{
+			//	MediaFolderPath = Path.Combine(RootPath, "Images");
+			//	isImage = true;
+			//}
+			//using (FileStream fileStreams = new(Path.Combine(MediaFolderPath,
+			//								FileName + Extension), FileMode.Create))
+			//{
+			//	media.CopyTo(fileStreams);
+			//	fileStreams.Flush();
+			//	fileStreams.Dispose();
+			//	fileStreams.Close();
+			//}
+			//if (isImage)
+			//	return @$"{_httpContext.HttpContext?.Request.Scheme}://{_httpContext?.HttpContext?.Request.Host}/Images/" + FileName + Extension;
+			//else
+			//	return @$"{_httpContext.HttpContext?.Request.Scheme}://{_httpContext?.HttpContext?.Request.Host}/Records/" + FileName + Extension;
 		}
 		public async Task<bool> DeleteAsync(string? url)
 		{
@@ -52,20 +73,17 @@ namespace ISC.Services.Services
 			}
 			try
 			{
-				string RootPath = _Host.WebRootPath.Replace("\\\\", "\\");
-				var imageNameToDelete = Path.GetFileNameWithoutExtension(url);
-				var EXT = Path.GetExtension(url);
-				string? oldPath = "";
-				if (isImageExtension(EXT))
-					oldPath = $@"{RootPath}\Images\{imageNameToDelete}{EXT}";
-				else return false;
-				if (File.Exists(oldPath))
-				{
-					File.Delete(oldPath);
-					return true;
-				}
-				return false;
-			}
+                string RootPath = _host.WebRootPath.Replace("\\\\", "\\");
+                var imageNameToDelete = Path.GetFileNameWithoutExtension(url);
+                var EXT = Path.GetExtension(url);
+                var oldImagePath = $@"{RootPath}\Images\{imageNameToDelete}{EXT}";
+                if (File.Exists(oldImagePath))
+                {
+                    File.Delete(oldImagePath);
+                }
+                await Task.CompletedTask;
+				return true;
+            }
 			catch
 			{
 				return false;
@@ -73,32 +91,23 @@ namespace ISC.Services.Services
 		}
 		public async Task<string?> Update(string oldUrl, IFormFile newMedia)
 		{
-			var Result = GetPathAsync(newMedia).Result;
-			if (Result == null) return Result;
-			if (oldUrl == Result) return oldUrl;
-			var NewUrl = await AddAsync(newMedia);
-			if (NewUrl == null)
-			{
-				throw new Exception("Coudln't update photo");
-
-			}
-			await DeleteAsync(oldUrl);
-			return NewUrl;
-		}
-		private async Task<string?> GetPathAsync(IFormFile media)
-		{
-			if (media == null) return null;
-			string FileName = Guid.NewGuid().ToString();
-			string Extension = Path.GetExtension(media.FileName);
-			if (isImageExtension(Extension))
-			{
-				return @$"{_HttpContext.HttpContext?.Request.Scheme}://{_HttpContext?.HttpContext?.Request.Host}/Images/" + FileName + Extension;
-			}
-			else
-			{
-				return null;
-			}
-		}
+            await DeleteAsync(oldUrl);
+            return await AddAsync(newMedia);
+        }
+		//private async Task<string?> GetPathAsync(IFormFile media)
+		//{
+		//	if (media == null) return null;
+		//	string FileName = Guid.NewGuid().ToString();
+		//	string Extension = Path.GetExtension(media.FileName);
+		//	if (isImageExtension(Extension))
+		//	{
+		//		return @$"{_httpContext.HttpContext?.Request.Scheme}://{_httpContext?.HttpContext?.Request.Host}/Images/" + FileName + Extension;
+		//	}
+		//	else
+		//	{
+		//		return null;
+		//	}
+		//}
 		private bool isImageExtension(string extension)
 		{
 			return extension == ".jpg" || extension == ".jpeg" || extension == ".jpe";
